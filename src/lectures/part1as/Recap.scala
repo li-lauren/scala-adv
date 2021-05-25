2c5ed7ea1f574c87a42cad6a721a100ed86fc608
li-lauren <laurenlidev@gmail.com>
2021-05-25 09:53:16 -0600

    Finish Partial Function

--- src/lectures/part1as/Recap.scala
+++ src/lectures/part1as/Recap.scala
@@ -2,5 +2,5 @@ package lectures.part1as
 
 object Recap extends App{
   val aCondition: Boolean = false
-  val acConditio
+
 }
---
+++ src/lectures/part2afp/PartialFunctions.scala
@@ -0,0 +1,84 @@
+package lectures.part2afp
+
+object PartialFunctions extends App {
+  val aFunction = (x: Int) => x + 1 // Function1[Int, Int] === Int => Int
+
+  val aFussyFunction = (x: Int) =>
+    if (x == 1) 42
+    else if (x == 2) 56
+    else if (x == 5) 999
+    else throw new FunctionNotApplicableException
+
+  class FunctionNotApplicableException extends RuntimeException
+
+  val aNicerFussyFunction = (x: Int) => x match {
+    case 1 => 42
+    case 2 => 56
+    case 5 => 999
+  }
+  // {1,2,5} => Int
+
+  val aPartialFunction: PartialFunction[Int, Int] = {
+    case 1 => 42
+    case 2 => 56
+    case 5 => 999
+  } // partial function value
+
+  // PF utilities
+  println(aPartialFunction.isDefinedAt(67))
+
+  // lift
+  val lifted = aPartialFunction.lift // Int => Option
+  println(lifted(2))
+  println(lifted(98))
+
+  // or else
+  val pfChain = aPartialFunction.orElse[Int, Int] {
+    case 45 => 67
+  }
+
+  println(pfChain(2))
+  println(pfChain(45))
+
+  // PF extend normal functions
+  val aTotalFunction: Int => Int = {
+    case 1 => 99
+  } // a declared total function can take a partial function value b/c
+  // partial functions are a subtype of total functions
+
+  // HOFs accept partial functions as well
+  val aMappedList = List(1,2,3).map {
+    case 1 => 42
+    case 2 => 78
+    case 3 => 1000
+  }
+
+  /*
+   Note: partial functions can only have ONE parameter type
+   */
+
+  /**
+   * Exercises
+   *
+   * 1  - construct a PF instance yourself (anonymous class)
+   * 2 - dumb chatbot as a PF
+   */
+
+  val aManualFussyFunction = new PartialFunction[Int, Int] {
+    override def apply(v1: Int): Int = x match {
+      case 1 => 42
+      case 2 => 65
+      case 5 => 999
+    }
+
+    override def isDefinedAt(x: Int): Boolean =
+      x == 1 || x == 2 || x == 5
+  }
+
+  val chatbot: PartialFunction[String, String] = {
+    case "hello" => "Hi, my name is blah"
+    case "goodbye" => "bye"
+  }
+
+  scala.io.Source.stdin.getLines().map(chatbot).foreach(println)
+}
